33413feab46cbb93fb8f68c51621c2b0f171b0c5
Shubham Anand <109297009+Shubham4835anand@users.noreply.github.com>
2024-08-14 08:38:25 +0530

    File Uploaded

---
+++ Coding Contents/content/Untitled1.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+	
+int a = 100;
+int b = 200;
+int c = 30;
+
+if(a>b){
+	
+if(a>c){
+
+cout <<"a is the largest";
+
+}
+
+}
+else{
+	
+if(b>c){
+
+cout <<"b is the largest";
+
+}
+
+else{
+
+cout <<"c is the largest";
+
+}
+
+return 0;
+}
+
+}
+
+
+
